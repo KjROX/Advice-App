e6c31fe9dd8b6537d8b04b5610bc4bf1f7717de6
KjROX <karnamandal380@gmail.com>
2022-05-02 21:53:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const gyaanNum = document.querySelector(".wrapper p span");
+const gyaan = document.querySelector(".wrapper h1");
+const button = document.querySelector(".round");
+const api = `https://api.adviceslip.com/advice`;
+
+function changeAdvise() {
+  fetch(api)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      //   console.log(data);
+      gyaan.textContent = data.slip.advice;
+      gyaanNum.textContent = data.slip.id;
+    })
+    .catch((err) => console.log(`Sorry😢,Something went wrong!`));
+}
+
+window.addEventListener("load", changeAdvise);
+button.addEventListener("click", changeAdvise);
